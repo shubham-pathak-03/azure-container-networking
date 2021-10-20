59ddfbb4357fcb6321565f00e04cd6d4329027a7
Vamsi Kalapala <vkalapa@ncsu.edu>
2021-10-20 13:03:26 -0700

    fixing a build issue

--- npm/pkg/dataplane/policies/policymanager_linux.go
+++ npm/pkg/dataplane/policies/policymanager_linux.go
@@ -1,9 +1,9 @@
 package policies
 
-func (pMgr *PolicyManager) addPolicy(policy *NPMNetworkPolicy, _ []string) error {
+func (pMgr *PolicyManager) addPolicy(policy *NPMNetworkPolicy, _ map[string]string) error {
 	return nil
 }
 
-func (pMgr *PolicyManager) removePolicy(name string, _ []string) error {
+func (pMgr *PolicyManager) removePolicy(name string, _ map[string]string) error {
 	return nil
 }
